ca5839c8c824d6f3c5a7506434bb77c015f1268f
Sunny <ring2rings93@gmail.com>
2019-04-11 22:41:26 +0100

    tie case and some refactor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require './lib/game.rb'
 
 describe Game do
   let(:game) { Game.new }
-  let(:player) { double(name: 'Oso', board_piece: 'X')}
+  let(:player) { double(name: 'Oso', board_piece: 'X') }
   describe '#win?' do
     context "with a row of 'X'" do
       let(:state) { ['X', 'X', 'X', '-', '-', '-', '-', '-', '-'] }
@@ -26,15 +28,40 @@ describe Game do
         expect(game.win?(player, state)).to eq(true)
       end
     end
-  end
-  describe '#tie?' do
-    describe "with board full" do
-      it "return true" do
+
+    context "with a diagonal of 'X false cases'" do
+      let(:state) { ['O', '-', '-', '-', 'X', '-', '-', '-', 'X'] }
+      it 'return false' do
+        expect(game.win?(player, state)).to_not eq(true)
+      end
+      let(:state) { ['-', '-', 'X', '-', 'O', '-', 'X', '-', '-'] }
+      it 'return true' do
+        expect(game.win?(player, state)).to be false
       end
     end
-    describe "with board not full" do
-      it "return true" do
+  end
+
+  describe '#tie?' do
+    let(:pieces) { %w[O X] }
+    describe 'with board full' do
+      let(:state) { %w[X O X O X O O X O] }
+      it 'return true' do
+        expect(game.tie?(pieces, state)).to eql(true)
       end
+    end
+    describe 'with board not full' do
+      let(:state) do
+        ['X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', '-']
+      end
+      it 'return false' do
+        expect(game.tie?(pieces, state)).to eql(false)
+      end
+    end
+  end
+
+  describe '#end_game' do
+    it 'exits the game when called' do
+      expect(game.end_game).to eql(true)
     end
   end
 end
